1349351b8845e00921e3b82518414f942da551b0
CHANCHAO\Anthony <anthony@chanchao.com.tw>
2023-10-19 17:21:49 +0800

    註掉PM2設定 packge.json改為nuxt generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
 
   // mode: "universal",
-  target: "static",
+  // target: "static",
   // ssr: false,
 
   // googleAnalytics: {
